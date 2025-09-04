2c53e3df6d81db8247a0ab513a7ac06fe9766bbb
Ishan Bahuguna <ishanbahuguna1@gmail.com>
2025-09-04 16:38:26 +0530

    added busCardSkeleton component and animations

---
+++ app/types/index.ts
@@ -0,0 +1,8 @@
+export interface BusCardProps {
+    index:number,
+    busNo: string;
+    routeName:string;
+    startTime: string;
+    endTime: string;
+    est: string;
+}
--- styles/route.styles.ts
+++ styles/route.styles.ts
@@ -6,7 +6,7 @@ const {height , width} = Dimensions.get("window")
 export const styles = StyleSheet.create({
     container: {
        flex:1,
-       backgroundColor:COLORS.background
+       backgroundColor:COLORS.background,
        
     }
 })
